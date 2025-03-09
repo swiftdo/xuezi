e7e39ea47c6edc49094e311a0b8dacd6510a440d
laijihua <1164258202@qq.com>
2025-03-09 19:42:48 +0800

    导出对接

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -24,10 +24,74 @@ class _MyHomePageState extends State<MyHomePage> {
     '十'
   ];
 
+  final List<String> firstCharacters = [
+    "天",
+    "地",
+    "人",
+    "你",
+    "我",
+    "他",
+    "一",
+    "二",
+    "三",
+    "四",
+    "五",
+    "上",
+    "下",
+    "口",
+    "耳",
+    "目",
+    "手",
+    "足",
+    "站",
+    "坐",
+    "日",
+    "月",
+    "水",
+    "火",
+    "山",
+    "石",
+    "田",
+    "禾",
+    "对",
+    "云",
+    "雨",
+    "风",
+    "花",
+    "鸟",
+    "虫",
+    "六",
+    "七",
+    "八",
+    "九",
+    "十",
+    "爸",
+    "妈",
+    "马",
+    "土",
+    "不",
+    "画",
+    "打",
+    "棋",
+    "鸡",
+    "字",
+    "词",
+    "语",
+    "句",
+    "桌",
+    "纸",
+    "妹",
+    "奶",
+    "小",
+    "桥",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    List<String> shuffledCharacters = List<String>.from(characters1To10)..shuffle();
+    List<String> shuffledCharacters = List<String>.from(characters1To10)
+      ..shuffle();
+
+    List<String> firstShuffledCharacters = firstCharacters..shuffle();
 
     return Scaffold(
       appBar: AppBar(
@@ -47,6 +111,27 @@ class _MyHomePageState extends State<MyHomePage> {
               onTap: () {
                 context.push('/character-swiper', extra: {
                   'characters': shuffledCharacters,
+                  'flutterTts': flutterTts,
+                });
+                // context.go('/character-swiper', extra: {
+                //   'characters': shuffledCharacters,
+                //   'flutterTts': flutterTts,
+                // });
+              },
+            ),
+          ),
+          Card(
+            color: Colors.blueAccent,
+            child: ListTile(
+              title: const Center(
+                child: Text(
+                  '一级汉字',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              onTap: () {
+                context.push('/character-swiper', extra: {
+                  'characters': firstShuffledCharacters,
                   'flutterTts': flutterTts,
                 });
                 // context.go('/character-swiper', extra: {
